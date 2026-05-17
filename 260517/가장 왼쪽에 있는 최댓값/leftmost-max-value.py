8d7da56ef915d6e9df6cb77577125134c0cfda15
jungssg <131239981+jungssg@users.noreply.github.com>
2026-05-17 18:32:15 +0900

    [Passed] 가장 왼쪽에 있는 최댓값 (55ms, 17MB)

---
+++ 260517/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,23 @@
+# 최대값 설정 => 가장 왼쪽 인덱스
+# 다시 최대값 설정 => 가장 왼쪽 인덱스
+n = int(input())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+answer = []
+max_val = max(a)
+max_idx = a.index(max_val)
+answer.append(max_idx + 1)
+
+for i in range(n):
+    if max_idx == 0:
+        break;
+    else:
+        max_val = max(a[:max_idx])
+        max_idx = a[:max_idx].index(max_val)
+        answer.append(max_idx + 1)
+
+for num in answer:
+    print(num, end=' ')
+
+
