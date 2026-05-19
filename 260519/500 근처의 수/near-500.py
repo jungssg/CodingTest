12dc5738c04f6e7ad9d0bc5510c2c2b1ca774f29
jungssg <131239981+jungssg@users.noreply.github.com>
2026-05-19 17:25:55 +0900

    [Passed] 500 근처의 수 (45ms, 16MB)

---
+++ 260519/500 근처의 수/near-500.py
@@ -0,0 +1,13 @@
+# 초기 500 초과 중 가장 작은 수, 500 미만 중 가장 큰 수 설정
+nums = list(map(int, input().split()))
+
+pre_min_num = 0
+pre_max_num = 1000
+for num in nums:
+    if num < 500:
+        if num > pre_min_num:
+            pre_min_num = num
+    else:
+        if num < pre_max_num:
+            pre_max_num = num
+print(pre_min_num, pre_max_num)
