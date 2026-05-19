5ec679e9ad3d12b4bdc3a75efec61e3414abb034
jungssg <131239981+jungssg@users.noreply.github.com>
2026-05-19 18:06:39 +0900

    [Passed] 두 수의 차의 최솟값 (44ms, 16MB)

--- 260519/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 260519/두 수의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -1,12 +1,12 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
+# 차이 초기값 설정
 min_val = 100
 
-for i in range(n):
-    for j in range(i+1, n):
-        # 정수는 오름차순으로 정렬되어 있기 때문에 뒤에 수가 무조건 더큼
-        val = nums[j] - nums[i]
-        if val > 0 and val < min_val:
-            min_val = val
+# 정수는 오름차순으로 정렬되어 있기 때문에 뒤에 수가 무조건 더큼
+for i in range(1,n):
+    val = nums[i] - nums[i-1]
+    if val < min_val :
+        min_val = val
 print(min_val)        
